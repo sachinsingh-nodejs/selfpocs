7e56f2378d41832701730f007b6d2bc09ff8befc
Sachin Singh <ssingh259@sapient.com>
2016-12-23 18:09:56 +0530

    Basic responsive navigation

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let app = express();
 // app.use(express.static('public'));
 app.set("views", path.join(__dirname, "/public"));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+app.use("/lib", express.static(__dirname + "/bower_components"));
 app.get("/", function (req, res) {
     res.render("index");
 });
