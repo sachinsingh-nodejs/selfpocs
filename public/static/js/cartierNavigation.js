37f112ecf5ca404af83acd142eb9f8e319bea392
Sachin Singh <ssingh259@sapient.com>
2016-12-24 13:11:34 +0530

    Handled device orientation IE9+

--- public/static/js/cartierNavigation.js
+++ public/static/js/cartierNavigation.js
@@ -1,23 +1,45 @@
 $(function () {
-    var overlay = $(".js-navigation__overlay"),
+    var navigation = $("#jsNavigationToggle"),
+        overlay = $(".js-navigation__overlay"),
         menu = $(".js-navigation__container");
+    // Implement window resize event to dynamically analyze when to switch event on and off
+    $(window).on("resize", function () {
+        console.log("Window resized");
+        if ($(this).width() < 768) {
+            navigation.off("click").on("click", _toggleNavigation);
+            overlay.off("click").on("click", _overlayClick);
+            menu.off("click").on("click", _stopEvtPropagation);
+        } else {
+            // Everything is off
+            navigation.off("click");
+            overlay.off("click");
+            menu.off("click");
+        }
+    });
+    // Default page load
     if ($(window).width() < 768) {
-        $("#jsNavigationToggle").on("click", function (event) {
-            overlay.removeClass("hidden-xs");
-            setTimeout(function () {
-                overlay.addClass("color-transform");
-                menu.addClass("translated");
-            }, 1);
-        });
-        overlay.on("click", function () {
-            overlay.removeClass("color-transform");
-            menu.removeClass("translated");
-            setTimeout(function () {
-                overlay.addClass("hidden-xs");
-            }, 500); // Since transition runs for 500 ms
-        });
-        menu.on("click", function (event) {
-            event.stopPropagation();
-        });
+        navigation.on("click", _toggleNavigation);
+        overlay.on("click", _overlayClick);
+        menu.on("click", _stopEvtPropagation);
+    }
+
+    function _toggleNavigation () {
+        overlay.removeClass("hidden-xs");
+        setTimeout(function () {
+            overlay.addClass("color-transform");
+            menu.addClass("translated");
+        }, 10);
+    }
+
+    function _overlayClick () {
+        overlay.removeClass("color-transform");
+        menu.removeClass("translated");
+        setTimeout(function () {
+            overlay.addClass("hidden-xs");
+        }, 500); // Since transition runs for 500 ms
+    }
+
+    function _stopEvtPropagation (e) {
+        e.stopPropagation();
     }
 });
