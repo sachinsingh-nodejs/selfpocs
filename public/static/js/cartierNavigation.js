69809c1ab484bcd71097f3d8ecf060869b095d25
Sachin Singh <ssingh259@sapient.com>
2016-12-24 12:12:11 +0530

    Implemented left slide menu transition

---
+++ public/static/js/cartierNavigation.js
@@ -0,0 +1,23 @@
+$(function () {
+    var overlay = $(".js-navigation__overlay"),
+        menu = $(".js-navigation__container");
+    if ($(window).width() < 768) {
+        $("#jsNavigationToggle").on("click", function (event) {
+            overlay.removeClass("hidden-xs");
+            setTimeout(function () {
+                overlay.addClass("color-transform");
+                menu.addClass("translated");
+            }, 50);
+        });
+        overlay.on("click", function () {
+            overlay.removeClass("color-transform");
+            menu.removeClass("translated");
+            setTimeout(function () {
+                overlay.addClass("hidden-xs");
+            }, 500); // Since transition runs for 500 ms
+        });
+        menu.on("click", function (event) {
+            event.stopPropagation();
+        });
+    }
+});
